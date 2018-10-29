4166fa4a50d11a0a768f14e72df030e54b20f3fe
grantfogle <grantjfogle@gmail.com>
2018-10-28 22:09:16 -0600

    made some additional changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -26,6 +26,19 @@ class Home extends Component {
                             <button type="button" class="btn btn-outline-primary">Contact</button>
                         </div>
                     </div>
+                    <div className="row">
+                        <div className="col-sm-12">
+                            <h2>Skills</h2>
+                            <p>
+                                <span className="badge badge-primary">HTML5</span>
+                                <span className="badge badge-primary">CSS3</span>
+                                <span className="badge badge-primary">Javascript</span>
+                                <span className="badge badge-primary">React</span>
+                                <span className="badge badge-primary">Redux</span>
+                                <span className="badge badge-primary">Express</span>
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </main>
         );
