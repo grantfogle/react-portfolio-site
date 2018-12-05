91aca26f201ca4be47bae2ff87e9a664745a7190
grantfogle <grantjfogle@gmail.com>
2018-12-04 20:50:46 -0700

    working on styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEnvelope, faKey } from '@fortawesome/free-solid-svg-icons';
+import brands from '@fortawesome/fontawesome-free-brands'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../App.css';
 
 class Footer extends Component {
     render() {
         return (
-            <footer className="page-footer font-small pt-4">
-                <div className="container-fluid text-center text-md-left">
+            <footer className="page-footer">
+                <div className="text-center">
                     <div className="row">
+                        <div className="col-sm-12">
+                            <a href="https://github.com/grantfogle/"><FontAwesomeIcon icon={["fab", "github"]} color="#fff" size="4x" className="icons" /></a>
+                            <a href="https://www.linkedin.com/in/grant-fogle-092b8698/"><FontAwesomeIcon icon={["fab", "linkedin"]} color="#fff" size="4x" className="icons" /></a>
+                            <a href="mailto:grantjfogle@gmail.com"><FontAwesomeIcon icon={faEnvelope} color="#fff" size="lg" size="4x" className="icons" /></a>
+                        </div>
                     </div>
                 </div>
                 <div className="footer-copyright text-center text-white py-3">© 2018 Copyright</div>
-            </footer>
+            </footer >
         );
     }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -8,25 +8,23 @@ class Home extends Component {
                 <div className="container">
                     <div className="row">
                         <div className="col-sm-12">
-                            <h1>Hey, It's Grant Here</h1>
+                            <h1>Hey, I'm Grant</h1>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-md-6 col-sm-12">
                             <div className="home-image">
-                                <img src="https://via.placeholder.com/300.png" />
+                                <img src="https://media.licdn.com/dms/image/C4E03AQH2I7CUuhnm3A/profile-displayphoto-shrink_200_200/0?e=1548892800&v=beta&t=QX5KKwJMv-oHLx7-nWb0iJYCCCd3adI6D08XuhwFA-c" />
                             </div>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-md-6 col-sm-12">
-                            <div className="side-bar-info">
-                                <h1 className="text-primary">Full Stack Developer</h1>
-                                <h4>Full Stack Developer</h4>
-                                <p>Developer, skier, shredder <br></br>I'm just out here havin' fun.</p>
-                            </div>
+                            <h4>Full Stack Developer</h4>
+                            <p>Developer, skier, shredder <br></br>I'm just out here havin' fun.</p>
+                            <p>My passion is creating functional apps that
+                                    help make the world a better place and people's lives easier.</p>
                         </div>
-
                     </div>
                     <div className="row">
                         <div class="ctas">
